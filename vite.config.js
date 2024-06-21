4b2eda2fb436df95322a849716eb5bbf277a4b85
DOREE VINCENT <vincent.doree01@gmail.com>
2024-06-21 14:48:52 +0200

    patch vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // If you are using React, similarly for other frameworks
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Vincent/",
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Directory where the production build will be output
+    sourcemap: false, // Generate source maps for debugging
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: Split vendor code into a separate chunk
+          vendor: ['react', 'react-dom']
+        }
+      }
+    },
+  },
+  server: {
+    // Configuration for development server if needed
+    port: 3000,
+    open: true,
+  },
+  
+});
