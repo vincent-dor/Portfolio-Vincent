f23d12f1574797f9bcd37ba29a3185596f406c76
DOREE VINCENT <vincent.doree01@gmail.com>
2024-06-21 13:56:36 +0200

    feat: add social link, header with nav and scss

---
+++ portfolio/src/components/Nav/index.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import Social from "../Social/index";
+
+const Nav = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const darkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(() => {
+    document.body.classList.add("light-theme");
+    if (isDarkMode) {
+      document.body.classList.add("dark-theme");
+      document.body.classList.remove("light-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+      document.body.classList.add("light-theme");
+    }
+  }, [isDarkMode]);
+
+  return (
+    <header className="header" id="Home">
+      <nav className={`nav ${isDarkMode ? "dark-theme" : "light-theme"}`}>
+        <a href="#Home" className="nav__brand">
+          <span className="nav__title">Vincent Dorée</span>
+        </a>
+        <Social />
+        <ul className="nav__lists">
+          <li className="nav__section">
+            <a href="#Home">Home</a>
+          </li>
+          <li className="nav__section">
+            <a href="#Projects">Projets</a>
+          </li>
+          <li className="nav__section">
+            <a href="#Competence">Compétences</a>
+          </li>
+          <li className="nav__section">
+            <a href="#Contact">Contact</a>
+          </li>
+          <a href="#" className="nav__dark-mode" onClick={darkMode}>
+            <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} className="icon" />
+          </a>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
